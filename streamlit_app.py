441f25653eee8eaf886a4c382c9cdded2695d0d4
dario_ceni <dario01ceni@gmail.com>
2023-05-15 16:37:24 +0200

    added firestore first example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from google.cloud import firestore
 
-st.header('hello!')
-if st.button('balloons?'):
-    st.balloons()
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+doc_ref = db.collection("posts").document("Google")
+
+doc = doc_ref.get()
+
+
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
